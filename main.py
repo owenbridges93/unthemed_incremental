11483f949f9a0c823bfd4a203ff5a267bd7ac232
owenpoints <thomsonfrankford@duck.com>
2025-03-15 11:29:20 -0400

    reorganized functions, changed file structure, added more options for buy amount

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import ast
 import random
 import time
 
-os.system("cls")
-
 def pretty_num(num):
 
     num = safe_int(num)
@@ -41,11 +39,6 @@ def safe_int(num):
     if int(num) == num:
         num = int(num)
     return num
-
-def save():
-    save_file = open('save', 'w', encoding = "utf-8")
-    save_file.write(str(save_data))
-    save_file.close()
 
 def click(times = 1):
     clicks = []
@@ -88,7 +81,10 @@ def calc_upgrade_message(upgrade_type, cost):
         "cm" : "Critical Click Multiplier",
     }
 
-    message = f"Upgraded {messages[upgrade_type]} for {pretty_num(cost)} points."
+    if save_data["current_buy_amount"] == 1:
+        message = f"Upgraded {messages[upgrade_type]} for {pretty_num(cost)} points."
+    else:
+        message = f'Upgraded {messages[upgrade_type]} {pretty_num(save_data["current_buy_amount"])} times for {pretty_num(cost)} points.'
 
     return message
     
@@ -108,17 +104,17 @@ def upgrade(upgrade_type):
         update_window()
         update_display_costs()
 
-def update_display_costs():
-    global next_upgrade_costs
-    next_upgrade_costs = {upgrade_type: determine_cost(upgrade_type) for upgrade_type in upgrade_types}
+def cycle_buy_amount():
+    global save_data
+    amounts = [1, 10, 25, 100, 1000]
+    save_data["current_buy_amount"] = amounts[(amounts.index(save_data["current_buy_amount"]) + 1) % len(amounts)]
+    update_window()
+    update_display_costs()
 
-    upgrade_buttons = [ppc_upgrade_button, pm_upgrade_button, acps_upgrade_button, cc_upgrade_button, cm_upgrade_button]
-    upgrade_names = ["PpC", "EP", "aCpS", "CC", "CM"]
-    for i in range(len(upgrade_buttons)):
-        upgrade_buttons[i].config(text = f'Upgrade {upgrade_names[i]} (Cost: {pretty_num(next_upgrade_costs[upgrade_types[i]])})', command = lambda: upgrade(upgrade_types[i]))
-
-def update_display_prices():
-    pass
+def toggle_autobuyer():
+    global save_data
+    save_data["autobuyer_state"] = not (save_data["autobuyer_state"])
+    update_window()
 
 def display_info():
     global info_window
@@ -139,7 +135,7 @@ def display_info():
     info_window.title("Information")
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    icon_path = os.path.join(script_dir, "info.ico")
+    icon_path = os.path.join(script_dir, "assets/media/info.ico")
     info_window.iconbitmap(icon_path)
 
     info_font = ("TkDefaultFont", 11)
@@ -166,35 +162,18 @@ def display_info():
 
     info_window.mainloop()
 
-def cycle_buy_amount():
-    global save_data
-    amounts = [1, 10, 25]
-    save_data["current_buy_amount"] = amounts[(amounts.index(save_data["current_buy_amount"]) + 1) % len(amounts)]
-    update_window()
-    update_display_costs()
-
-def toggle_autobuyer():
-    global save_data
-    save_data["autobuyer_state"] = not (save_data["autobuyer_state"])
-    update_window()
-
 def placeholder_function():
     print("Placeholder clicked.")
+
+def save():
+    save_file = open('assets/save', 'w', encoding = "utf-8")
+    save_file.write(str(save_data))
+    save_file.close()
 
 def quit_game():
     save_data['last_play_time'] = time.time()
     save()
     exit()
-
-def adjust_font_size():
-    window_width = window.winfo_width()
-    window_height = window.winfo_height()
-
-    font_size = max(math.ceil(min(window_width, window_height) / 25), 8)
-    new_font = ("TkDefaultFont", font_size)
-
-    text_style.configure("TLabel", font = new_font)
-    text_style.configure("TButton", font = new_font)
 
 def create_window():
     global points_text, pm_text, ppc_text, acps_text, cc_text, cm_text, eppc_text, pps_text
@@ -219,7 +198,7 @@ def create_window():
     root.title("Unthemed Incremental")
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    icon_path = os.path.join(script_dir, "icon.ico")
+    icon_path = os.path.join(script_dir, "assets/media/icon.ico")
     root.iconbitmap(icon_path)
 
     text_style = ttk.Style()
@@ -295,10 +274,33 @@ def update_window():
     cba_button.config(text = f'Cycle Buy Amount (Current: {save_data["current_buy_amount"]})', command = cycle_buy_amount)
 
     autobuyer_states = {False: "Off", True: "On"}
-    autobuyer_button.config(text = f'Toggle Autobuyer (Is: {autobuyer_states[save_data["autobuyer_state"]]})', command = toggle_autobuyer)
+    autobuyer_button.config(text = f'Toggle Autobuyer (State: {autobuyer_states[save_data["autobuyer_state"]]})', command = toggle_autobuyer)
 
-if os.path.exists('save'):
-    save_file = open('save', 'r', encoding = "utf-8")
+def adjust_font_size():
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+
+    font_size = max(math.ceil(min(window_width, window_height) / 25), 8)
+    new_font = ("TkDefaultFont", font_size)
+
+    text_style.configure("TLabel", font = new_font)
+    text_style.configure("TButton", font = new_font)
+
+def update_display_costs():
+    global next_upgrade_costs
+    next_upgrade_costs = {upgrade_type: determine_cost(upgrade_type) for upgrade_type in upgrade_types}
+
+    upgrade_buttons = [ppc_upgrade_button, pm_upgrade_button, acps_upgrade_button, cc_upgrade_button, cm_upgrade_button]
+    upgrade_names = ["PpC", "EP", "aCpS", "CC", "CM"]
+    for i in range(len(upgrade_buttons)):
+        upgrade_buttons[i].config(text = f'Upgrade {upgrade_names[i]} (Cost: {pretty_num(next_upgrade_costs[upgrade_types[i]])})', command = lambda: upgrade(upgrade_types[i]))
+
+
+
+os.system("cls")
+
+if os.path.exists('assets/save'):
+    save_file = open('assets/save', 'r', encoding = "utf-8")
     save_data = ast.literal_eval(save_file.read())
     save_file.close()
     expected_points_per_click = round(save_data["pm"] * save_data["ppc"] * (((1 - save_data["cc"] % 1) * save_data["cm"] ** math.floor(save_data["cc"])) + (save_data["cc"] % 1) * save_data["cm"] ** math.ceil(save_data["cc"])))
@@ -306,13 +308,13 @@ if os.path.exists('save'):
     print(f"Gained {pretty_num(offline_points)} points while offline.")
     save_data["points"] += offline_points
 else:
-    save_file = open('save', 'x', encoding = "utf-8")
+    save_file = open('assets/save', 'x', encoding = "utf-8")
     save_file.close()
     # ppc = Points per click, pm = Points multiplier, acps = Autoclicks per second, cc = Critical chance, cm = Critical multiplier
-    save_data = {'points': 0, 'ppc': 1, 'pm': 1, 'acps': 0, 'cc': 0.01, 'cm': 10, 'ppc_cost' : 10, 'pm_cost' : 100, 'acps_cost' : 1000, 'cc_cost': 10000, 'cm_cost': 100000, 'current_buy_amount': 1, 'autobuyer_state': False, 'last_play_time': time.time()}
+    save_data = {'points': 0, 'ppc': 1, 'pm': 1, 'acps': 0, 'cc': 0.01, 'cm': 10, 'current_buy_amount': 1, 'autobuyer_state': False, 'last_play_time': time.time()}
 
 def game_loop():
-    global prev_points, points_per_second, expected_points_per_click
+    global prev_points, points_per_second
 
     click(save_data["acps"])
     points_per_second = save_data["points"] - prev_points
